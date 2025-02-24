66e4efa77ad0be9f80486e66431e156bfe1744d8
Felip <156847988+Felip-Torres@users.noreply.github.com>
2025-02-24 23:19:10 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,4 +1,9 @@
 import pymysql.cursors
+import hashlib
+import base64
+import os
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
 from datetime import datetime
 
 class database(object):
@@ -24,15 +29,6 @@ class database(object):
 
     def desconecta(self):
         self.db.close()
-
-    import hashlib
-import base64
-import os
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
-
-class database:
-    # Otras funciones de la clase...
 
     def hash_password(self, password: str) -> str:
         """Genera un hash seguro usando Scrypt."""
